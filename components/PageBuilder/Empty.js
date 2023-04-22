5fdb7f6b37bd0f652bdba70533bbec9e15bc7269
Wynter Jones <34955844+WynterJones@users.noreply.github.com>
2023-04-22 19:49:27 -0400

    .

--- components/PageBuilder/Empty.js
+++ components/PageBuilder/Empty.js
@@ -3,12 +3,11 @@
  *   All rights reserved.
  */
 
-export default function Empty({ message, className, onMouseOver }) {
+export default function Empty({ message, className }) {
   return (
     <div>
       <div
-        className={`border border-slate-200 text-sm p-6 rounded shadow-sm bg-slate-50 hover:bg-slate-100 text-center uppercase font-medium`}
-        // onMouseOver={onMouseOver}
+        className={`border border-slate-200 text-sm p-6 rounded shadow-sm bg-slate-50 hover:bg-slate-100 text-center uppercase font-medium ${className}`}
       >
         {message}
       </div>
